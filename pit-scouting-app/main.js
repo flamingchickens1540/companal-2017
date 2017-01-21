1fe9d5e45f1ffa2b41a2dc137690164daeaab0d5
Dylan Smith <smithd@catlin.edu>
2017-01-21 12:12:57 -0800

    Pit scouting first commit!!!

---
+++ pit-scouting-app/main.js
@@ -0,0 +1,19 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var ipc = require("electron").ipcMain;
+var fs = require('fs');
+
+app.on('ready', function () {
+	var mainWindow = new BrowserWindow({width:800,height:600})
+	mainWindow.loadURL('file://' + __dirname + '/index.html');
+	ipc.on('read-file', function () {
+		var contents = fs.readFileSync('./package.json', 'utf8');
+		alert(contents);
+	});
+});
+
+/* Other
+	used: https://www.youtube.com/watch?v=K-H2amwQ_pU
+	documentation: http://electron.atom.io/docs/api/browser-window/
+	mainWindow.openDevTools();
+*/
--- pit-scouting-app/script.js
+++ pit-scouting-app/script.js
@@ -16,43 +16,106 @@ function appendFile(one,two) {
 	});
 }
 
-var accounts = {"55":"Dylan",
-				"66":"Fin",
+function createTable() {
+	var keys = Object.keys(accounts);
+	for (x in keys) {
+		$("#tbody").append("<tr><td>"+accounts[keys[x]]+"</td><td>"+keys[x]+"</td></tr>");
+	}
+}
+function camera() {
+	var enabled = false;
+	var WebCamera = require("webcamjs");
+	document.getElementById("start").addEventListener('click',function(){
+  		if (!enabled) { // Start the camera !
+    		enabled = true;
+    		WebCamera.attach('#camdemo');
+    		console.log("The camera has been started");
+   		} else { // Disable the camera !
+    		enabled = false;
+    		WebCamera.reset();
+    		console.log("The camera has been disabled");
+    	}
+	},false);
+}
+
+
+var accounts = {"01":"Ben",
+				"05":"Nicholas",
+				"12":"Jonathan",
 				"15":"Tristan",
-				"40":"Ryan",
-				"96":"Alexander",
-				"31":"Jake",
-				"76":"Dale",
+				"18":"Liam W",
+				"19":"Andrei",
+				"20":"Holly",
+				"22":"Ava",
 				"24":"Amber",
-				"64":"Hammad",
-				"01":"Ben",
-				"13":"Liam B",
-				"72":"Josephine",
-				"98":"Adolfo",
+				"26":"Ruby",
+				"31":"Jake",
+				"34":"Spencer",
+				"37":"Aarushi",
+				"39":"Kean",
+				"40":"Ryan",
+				"42":"David",
+				"44":"Alexander Y",
+				"48":"Kobi",
+				"49":"Claire",
+				"51":"Lauren Mei",
+				"55":"Dylan",
 				"59":"Tyler",
+				"60":"Natalie",
+				"64":"Hammad",
+				"66":"Fin",
+				"70":"Bailey",
+				"71":"Culla",
+				"72":"Josephine",
+				"75":"Noor",
+				"79":"Hannah",
+				"82":"Marti",
+				"85":"Robin",
+				"92":"Zachary",
+				"95":"Quinn",
+				"96":"Alexander M",
+				"98":"Adolfo",
+				"99":"Liam B",
 				"00":"Anonymous Scout" 
 				};
-var logged = 0;
+var logged = false;
 var act = "none";
 var secact = "none";
 var team = "none";
+camera();
+
 
 $(document).ready(function(){
+	createTable();
 	$("#logbutton").click(function(){
 		var num = $("#lognum").val();
-		if (accounts.hasOwnProperty(num)) {
-			logged+=1;
-			if (logged==1) {
-				act=num;
-			} else if (logged==2) {
-				secact=num;
+		var numb = $("#lognumb").val();
+		if (accounts.hasOwnProperty(num) && accounts.hasOwnProperty(numb)) {
+			logged=true;
+			act=num;
+			secact=numb;
+			if (num==numb) {
+				$("#yourname").text(accounts[num]);
+				secact="-"
+			} else {
+				$("#yourname").text(accounts[num]+" and "+accounts[numb]);
 			}
-			$("#yourname").text(accounts[num]);
-			$("#check").animate({opacity:'1.0',top:'175'},1600);
+			$("#check").animate({opacity:'1.0',top:'235'},1600);
 		} else {
-			$("#noid").show();
+			var chosen;
+			if (!accounts.hasOwnProperty(num)) {
+				chosen=num;
+				$("#lognum").val("");
+			} else {
+				chosen=numb;
+				$("#lognumb").val("");
+			}
+			if (chosen=="") {
+				chosen="None";
+			}
+			$("#noid").show(chosen);
+			$("#alertid").text(chosen);
 		}
-		$("#lognum").val("");
 		$("#logname").val("");
 	});
 	$(".close").click(function(){
@@ -60,24 +123,20 @@ $(document).ready(function(){
 		$("#secondscout").hide();
 	});
 	$("#yes").click(function(){
-		if (logged==2) {
-			$("#top").animate({top:'-200px',opacity:'0.0'},1600);
-			$("#check").animate({opacity:'0.0',top:'-200px'},1600);
-			$("#teamsel").animate({opacity:'1.0',top:'0'},1600);
+		$("#top").animate({top:'-200px',opacity:'0.0'},1600);
+		$("#check").animate({opacity:'0.0',top:'-200px'},1600);
+		$("#teamsel").animate({opacity:'1.0',top:'0'},1600);
+		if (secact=="-") {
+			$(".title").text(accounts[act]);
+		} else {
 			$(".title").text(accounts[act]+", "+accounts[secact]);
-		} else if (logged==1) {
-			$("#secondscout").show();
-			$("#check").animate({top:'400px',opacity:'0.0'},1600);
 		}
 	});
 	$("#no").click(function(){
 		$("#check").animate({top:'400px',opacity:'0.0'},1600);
-		logged-=1;
-		if (logged==0) {
-			act="none";
-		} else if (logged==1) {
-			secact="none";
-		}
+		logged=false;
+		act="none";
+		secact="none";
 	});
 	$(".signout").click(function(){
 		$("#lognum").val("");
@@ -87,7 +146,7 @@ $(document).ready(function(){
 		$("#top").animate({opacity:'1.0',top:'0'},1600);
 		act="none";
 		secact="none";
-		logged=0;
+		logged=false;
 	});
 	$("#submit").click(function(){
 		var fs = require('fs');
