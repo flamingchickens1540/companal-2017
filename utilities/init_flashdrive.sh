af9a4eae2f1ae2cd506f557baddc4a16691b971e
Ryan Selden <seldenr@catlin.edu>
2017-03-08 22:13:45 -0800

    Added flash drive init script

---
+++ utilities/init_flashdrive.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#(C) 2017 Ryan Selden
+#This should be run on a Macintosh computer only.
+
+cd /Volumes/1540 || exit
+rm -rf /Volumes/1540/companal
+
+mkdir companal
+cd companal
+mkdir stand-scouting
+mkdir pit-scouting
+mkdir tournament
+mkdir output
+cd output/
+mkdir stand-scouting
+mkdir pit-scouting
+mkdir tournament
+
+cd ../..
+echo "This is the property of team 1540. We would be grateful if you could return it. Please call Ryan at (503) 828-4225. Thank you!!!" > PLEASE_RETURN.txt
+
+cd
+diskutil unmount /Volumes/1540
